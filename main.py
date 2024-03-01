432f44dc5b45dc6c62b9af4fab73760691b21169
danmtsu <daniel.martinstsuji@gmail.com>
2024-02-29 21:42:14 -0300

    avaliacoes

--- main.py
+++ main.py
@@ -3,21 +3,50 @@ from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+import pandas as pd
+from datetime import datetime
 from time import sleep
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-driver.get('https://economia.uol.com.br/cotacoes/bolsas/')
-sleep(2)
-input_busca = driver.find_element(By.ID, 'filled-normal')
+def get_data():
+    
 
-input_busca.send_keys('PETR3.SA')
-sleep(4)
-input_busca.send_keys(Keys.ENTER)
+    options = Options()
+    options.add_argument('--headless')
+    companys = ['PETR3.SA','MGLU3.SA','VIVT3.SA']
+    valores = list()
+    data_hora = list()
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-sleep(7)
-span_val = driver.find_element(By.XPATH, '//span[@class="chart-info-val ng-binding"]')
-cotacao_PETR = span_val.text
-print(f'valor petrobras = {cotacao_PETR}')
+    driver.get('https://economia.uol.com.br/cotacoes/bolsas/')
 
-input('')
+    for company in companys:
+        sleep(2)
+        input_busca = driver.find_element(By.ID, 'filled-normal')
+
+        input_busca.send_keys(company)
+        sleep(4)
+        input_busca.send_keys(Keys.ENTER)
+
+        sleep(7)
+        span_val = driver.find_element(By.XPATH, '//span[@class="chart-info-val ng-binding"]')
+        cotacao_company = span_val.text
+        valores.append(cotacao_company)
+        data_hora.append(datetime.now().strftime('%d/%m/%Y %H:%M:%S'))
+        print(f'valor {company} = {cotacao_company}')
+
+    dados = {
+        'company': companys,
+        'value': valores ,
+        'time_hour': data_hora
+    }
+    return dados
+
+def convert_to_excel(dados: dict, file_name:str):
+    df_companys = pd.DataFrame(dados)
+    df_companys.to_excel(f'./{file_name}.xlsx')
+
+
+data_company = get_data()
+convert_to_excel(data_company,"company")
